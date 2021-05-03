27e64df1910c27302d9dc68695f923835c9a042b
demeteradam <dmtr.adam99@gmail.com>
2021-05-03 14:13:30 +0200

    added insert to db

--- vasarlo.php
+++ vasarlo.php
@@ -35,6 +35,7 @@ error_reporting(E_ALL);
     </ul>
 </nav>
 <img id="customer" src="customer.png" alt="Customer" title="customer" width="350" height="250"  />
+
 <?php
 $tns = "
 (DESCRIPTION =
@@ -47,6 +48,52 @@ $tns = "
   )";
 
 $conn = oci_connect('admin', 'valami420', $tns,'UTF8');
+?>
+
+<form action="vasarlo.php" method="post">
+
+    <input class="" type="text" name="teljesNevVasarlo" value="" placeholder="Teljes név">
+    <input class="" type="text" name="szuletesiDatum" value="" placeholder="Születési dátum">
+    <input class="" type="text" name="lakcim" value="" placeholder="Lakcím">
+    
+    <button type="submit">Sor felvétele</button>
+</form>
+
+<?php
+
+if(!empty($_POST)) {
+    $msg="";
+
+    if(empty($_POST['teljesNevVasarlo']) || empty($_POST['szuletesiDatum']) || empty($_POST['lakcim'])) {
+        // TODO set error message
+        $msg.="<li>Az összes mező kitöltése kötelező";
+    }
+
+    if($msg!="") {
+        //TODO show errors after redirect
+        //header("location:vasarlo.php?error=".$msg);
+    } else {
+        $full_name = $_POST['teljesNevVasarlo'];
+        $birth_date = date('Y-m-d', strtotime(@$_POST['szuletesiDatum']));
+        $address = $_POST['lakcim'];
+        
+        //TODO: generate loginid
+        $sql = 'INSERT INTO VASARLO(loginid,teljesNevVasarlo,szuletesiDatum,lakcim) '.
+               'VALUES(0001, :teljesNevVasarlo, :szuletesiDatum, :lakcim)';
+
+        $compiled = oci_parse($conn, $sql);
+
+        oci_bind_by_name($compiled, ':teljesNevVasarlo', $full_name);
+        oci_bind_by_name($compiled, ':szuletesiDatum', $birth_date);
+        oci_bind_by_name($compiled, ':lakcim', $address);
+
+        $dateformat = oci_parse($conn, "alter session set nls_date_format='YYYY-MM-DD'");
+        oci_execute($dateformat);
+        oci_execute($compiled);
+
+        //header("location:vasarlo.php?ok=1");
+    }
+}
 
 echo '<h2>A tábla rekordjai: </h2>';
 echo '<table border="0" id="tabla">';
@@ -65,11 +112,8 @@ echo '</tr>';
 //$stid = oci_parse($conn, 'SELECT loginid, teljesNevVasarlo, szuletesiDatum, lakcim FROM VASARLO');
 oci_execute($stid);
 
-// Ez valamiért nem mükszik
 while ( ($row = oci_fetch_array($stid, OCI_ASSOC)) != false) {
-    echo '<tr>';
-    echo 'asd';
-    foreach ($row as $item) {
+    echo '<tr>';    foreach ($row as $item) {
         echo '<td style="text-align: center">' . $item . '</td>';
         
     }
