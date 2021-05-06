bbf1186c6649b4d90030f11a19676ac0b6117d62
tajtisanyi <83597793+tajtisanyi@users.noreply.github.com>
2021-05-06 11:12:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,21 +8,6 @@ session_start();
     <meta name="author" content="Demeter Ádám, Tajti Sándor, Bodnár Péter"/>
     <link rel="icon" href="images/logo.png"/>
     <link rel=stylesheet type="text/css" href="styles/index_styles.css" />
-    <style>
-        #udv{
-            position: relative;
-            display: block;
-            margin-left: auto;
-            margin-right: auto;
-        }
-
-        p {
-            color: white;
-            font-family: Arial, sans-serif;
-            font-size: 30px;
-            text-align: center;
-        }
-    </style>
 </head>
 <body>
 <header>
@@ -30,10 +15,10 @@ session_start();
 </header>
 <nav id="navigation">
     <ul>
-        <li><a href = index.php target="_self">Főoldal</a></li>
-        <li><a href = vasarlo.php target="_self">Vásárlóink</a></li>
-        <li><a href = books.php target="_self">Könyveink</a></li>
-        <li><a href = salers.php target="_self">Eladóink</a></li>
+        <li class="indexbtn"><a href = index.php target="_self">Főoldal</a></li>
+        <li class="vasarlobtn"><a href = vasarlo.php target="_self">Vásárlóink</a></li>
+        <li class="booksbtn"><a href = books.php target="_self">Könyveink</a></li>
+        <li class="salersbtn"><a href = salers.php target="_self">Eladóink</a></li>
     </ul>
 </nav>
 <p>Az alábbi Adatbázis alapú rendszerek gyakorlat projektmunka egy könyvesbolt webes felületének működését reprezentálja.</p>
