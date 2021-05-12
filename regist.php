d80dfdac6c11f8a459871459ddbb69dc360dd566
tajtisanyi <83597793+tajtisanyi@users.noreply.github.com>
2021-05-12 12:08:44 +0200

    Update regist.php

--- regist.php
+++ regist.php
@@ -1,13 +1,23 @@
 <?php
 
 ?>
-
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <title>Login Konyvesbolt</title>
+    <meta name="author" content="Demeter Ádám, Tajti Sándor, Bodnár Péter"/>
+    <link rel="icon" href="images/logo.png"/>
+    <link rel=stylesheet type="text/css" href="styles/reg_styles.css" />
+</head>
+<body>
 <form action="controllers/regist/insert.php" method="post">
+    <input class="" type="text" name="felhasznaloNev" value="" placeholder="felhasználó név"></br></br>
+    <input class="" type="email" name="email" value="" placeholder="emil"></br></br>
+    <input class="" type="password" name="pswd" value="" placeholder="jelszó"></br></br>
+    <input class="" type="password" name="pswdr" value="" placeholder="jelszó ismét"></br></br>
+    <button class="btn" type="submit">Regisztráció</button>
 
-    <input class="" type="text" name="felhasznaloNev" value="" placeholder="felhasználó név">
-    <input class="" type="email" name="email" value="" placeholder="emil">
-    <input class="" type="password" name="pswd" value="" placeholder="jelszó">
-    <input class="" type="password" name="pswdr" value="" placeholder="jelszó ismét">
-
-    <button type="submit">Sor felvétele</button>
-</form>
+</form></br>
+<button class="btn" type="submit">Bejelentkezés</button>
+</body>
+</html>
